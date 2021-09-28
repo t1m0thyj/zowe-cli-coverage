b0257bda16abdde877942e6f6efa859f8d8c4f12
Timothy Johnson <timothy.johnson@broadcom.com>
2021-09-28 12:33:11 -0400

    Collect coverage data and save to csv file
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 const exec = require("@actions/exec");
 const gitClone = require("git-clone/promise");
 const jsYaml = require("js-yaml");
-// const parseLcov = require("parse-lcov");
+const parseLcov = require("parse-lcov");
+const uglifyJs = require("uglify-js");
 
 (async () => {
     const config = jsYaml.load(fs.readFileSync("config.yaml", "utf-8"));
+    const csvLines = ["Project,Test Type,# Tests,% Coverage,Covered Lines,Total Lines"];
 
     for (const [repo, tests] of Object.entries(config.projects)) {
         const tempDir = fs.mkdtempSync("zowe");
@@ -14,15 +16,45 @@ const jsYaml = require("js-yaml");
         await exec.exec("npm", ["run", "build"], { cwd: tempDir });
 
         for (const [k, v] of Object.entries(tests)) {
-            const npmArgs = ["run", `test:${k}`];
-            if (typeof v === "boolean" && v) {
-                npmArgs.push("--", "--listTests");
+            const collectCoverage = !(typeof v === "boolean" && v);
+            if (collectCoverage && typeof v !== "string") {
+                continue;
             }
-            await exec.exec("npm", npmArgs, { cwd: tempDir });
+
+            if (collectCoverage) {
+                const output = await exec.getExecOutput("npm", ["run", `test:${k}`], { cwd: tempDir });
+                const numTests = parseInt(output.stdout.match(/^Tests:\s+\d+ passed, (\d+) total/)[1]);
+                const lcovFile = path.join(tempDir, v, "lcov.info");
+                const lcovInfo = parseLcov(fs.readFileSync(lcovFile, "utf-8"));
+                let foundLines = 0;
+                let hitLines = 0;
+                for (const record of lcovInfo) {
+                    foundLines += record.found;
+                    hitLines += record.hit;
+                }
+                csvLines.push(`${repo},${k},${numTests},${hitLines / foundLines * 100},${hitLines},${foundLines}`);
+            } else {
+                const output = await exec.getExecOutput("npm", ["run", `test:${k}`, "--", "--listTests"], { cwd: tempDir });
+                let numTests = 0;
+                for (const testFile in output.stdout.split("\n")) {
+                    const testContents = fs.readFileSync(testFile, "utf-8");
+                    const minTestContents = uglifyJs.minify(testContents).code;
+                    numTests += (minTestContents.match(/\bit\(/g) || []).length;
+                }
+                csvLines.push(`${repo},${k},${numTests}`);
+            }
         }
 
         fs.rmdirSync(tempDir, { recursive: true, force: true });
     }
+
+    const date = new Date();
+    const year = date.getFullYear().toString();
+    const month = ("0" + date.getMonth().toString()).slice(-2);
+    const day = ("0" + date.getDay().toString()).slice(-2);
+    const dateDir = path.join("results", year, month);
+    fs.mkdirSync(dateDir, { recursive: true });
+    fs.writeFileSync(path.join(dateDir, `${day}.csv`), csvLines.join("\n"));
 })().catch((error) => {
     console.error(error);
     process.exit(1);
