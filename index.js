1b23885909cc7405342d569b54b2a0335c47c70a
Timothy Johnson <timothy.johnson@broadcom.com>
2021-09-28 11:18:20 -0400

    Pass command arguments as separate list
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ const jsYaml = require("js-yaml");
     for (const [repo, tests] of Object.entries(config.projects)) {
         const tempDir = fs.mkdtempSync("zowe");
         await gitClone(`https://github.com/${repo}.git`, tempDir);
-        await exec.exec("npm install", { cwd: tempDir });
-        await exec.exec("npm run build", { cwd: tempDir });
+        await exec.exec("npm", ["install"], { cwd: tempDir });
+        await exec.exec("npm", ["run", "build"], { cwd: tempDir });
 
         for (const [k, v] of Object.entries(tests)) {
-            const jestArgs = (typeof v === "boolean" && v) ? "-- --listTests" : "";
-            await exec.exec(`npm run test:${k} ${jestArgs}`, { cwd: tempDir });
+            const npmArgs = ["run", `test:${k}`];
+            if (typeof v === "boolean" && v) {
+                npmArgs.push("--", "--listTests");
+            }
+            await exec.exec("npm", npmArgs, { cwd: tempDir });
         }
 
         fs.rmdirSync(tempDir, { recursive: true, force: true });
