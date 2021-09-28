4ab3d6ba6ba36ce44c2f2c80453c625ead5f7cd1
Timothy Johnson <timothy.johnson@broadcom.com>
2021-09-28 14:44:19 -0400

    Ignore npm scripts and install in ci mode
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const stripComments = require("strip-comments");
     for (const [repo, tests] of Object.entries(config.projects)) {
         const tempDir = fs.mkdtempSync("zowe");
         await gitClone(`https://github.com/${repo}.git`, tempDir);
-        await exec.exec("npm", ["install"], { cwd: tempDir });
+        fs.appendFileSync(path.join(tempDir, ".npmrc"), "ignore-scripts=true");
+        await exec.exec("npm", ["ci"], { cwd: tempDir });
         await exec.exec("npm", ["run", "build"], { cwd: tempDir });
 
         for (const [k, v] of Object.entries(tests)) {
