5c2a9f94dd0de5ab1beca24959c092a7f7b63a9e
Timothy Johnson <timothy.johnson@broadcom.com>
2021-09-28 10:56:09 -0400

    Initial commit
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const exec = require("@actions/exec");
+const gitClone = require("git-clone/promise");
+const jsYaml = require("js-yaml");
+// const parseLcov = require("parse-lcov");
+
+(async () => {
+    const config = jsYaml.load("config.yaml");
+
+    for (const [repo, tests] of Object.entries(config.projects)) {
+        const tempDir = fs.mkdtempSync("zowe");
+        await gitClone(`https://github.com/${repo}.git`, tempDir);
+        await exec.exec("npm install", { cwd: tempDir });
+        await exec.exec("npm run build", { cwd: tempDir });
+
+        for (const [k, v] of Object.entries(tests)) {
+            const jestArgs = (typeof v === "boolean" && v) ? "-- --listTests" : "";
+            await exec.exec(`npm run test:${k} ${jestArgs}`, { cwd: tempDir });
+        }
+
+        fs.rmdirSync(tempDir, { recursive: true, force: true });
+    }
+})();
